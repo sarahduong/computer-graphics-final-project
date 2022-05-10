5589a9cc180e47e9434319b1c2be34c8b589bb61
Grant Teters <51384311+gteters@users.noreply.github.com>
2022-05-10 17:25:25 -0500

    Add work in progress procedural mesh and walls/roof

---
+++ unity-project/Assets/Editor/Grid03Editor.cs
@@ -0,0 +1,16 @@
+﻿using UnityEngine;
+using UnityEditor;
+
+[CustomEditor(typeof(Mesh03))]
+public class Grid03Editor : Editor
+{
+    public override void OnInspectorGUI()
+    {
+        base.OnInspectorGUI();
+
+        Mesh03 scr = (Mesh03)target;
+        scr.OutlineGrid();        
+        scr.UpdateGridDisplay();
+        scr.SetCurrentCell();
+    }
+}

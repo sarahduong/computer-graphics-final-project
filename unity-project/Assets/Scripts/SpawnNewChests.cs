fc724f7a17efd8f4c1f21a1415448a25178e37a3
sarahduong <sd7my@umsystem.edu>
2022-05-12 15:56:31 -0500

    Add Game Manu Scene and UI

--- unity-project/Assets/Scripts/SpawnNewChests.cs
+++ unity-project/Assets/Scripts/SpawnNewChests.cs
@@ -8,10 +8,14 @@ public class SpawnNewChests : MonoBehaviour
     // Start is called before the first frame update
     void Start()
     {
-        for (int i = 0; i < 3; i++)
-        { 
+        for (int i = 0; i < 8; i++)
+        {
             var position = new Vector3(Random.Range(5.0f, 245.0f), 0.2f, Random.Range(5.0f, 245.0f));
-            Instantiate(treasure, position, Quaternion.identity);
+
+            Vector3 myVector = new Vector3(-90, 0, 0);
+
+
+            Instantiate(treasure, position, Quaternion.Euler(myVector));
         }
     }
 }
